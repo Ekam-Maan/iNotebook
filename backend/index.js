d67e784d8d5aa3a0e4dca00635e6455eccb67112
Ekam <ekamjot.nmed@gmail.com>
2023-06-26 22:18:32 -0400

    added get

--- backend/index.js
+++ backend/index.js
@@ -12,9 +12,9 @@ app.use(cors())
 
 app.use('/api/auth', require('./routes/auth.js'));
 app.use('/api/notes', require('./routes/notes.js'));
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
